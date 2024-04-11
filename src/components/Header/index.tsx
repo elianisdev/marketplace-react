a655c29f6795fb0b8503838b21bea3e237d04324
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-04-11 12:00:14 -0500

    create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,47 @@
-import {Box, Grid} from "@mui/material";
+import {Box, Divider, Grid, Typography} from "@mui/material";
 import React from "react";
 
 type HeaderProps = {
-
+title: string;
+description: string;
+element?: React.ReactNode | null;
 }
-export const HeaderComponent: React.FC <HeaderProps> = () => {
+export const HeaderComponent: React.FC <HeaderProps> = ({ title, description, element}) => {
     return (
-        <Box>
-           <Grid>
+        <div>
+            <Box sx={{ width: "100%", height: "350px" }} >
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    sx={{ height: "100%" }}
+                >
+                    <Grid item xs={5}></Grid>
+                    <Grid
+                        container
+                        direction="column"
+                        justifyContent="center"
+                        alignItems="center"
+                        sx={{ height: "100%" }}
+                    >
+                        <Grid item>
+                            <Typography variant="h1">
+                                {title}
+                            </Typography>
+                        </Grid>
+                        <Grid item sx={{ mt: 2 }}>
+                            <Typography>
+                                {description}
+                            </Typography>
+                        </Grid>
+                        {element !== undefined && (
+                         <Grid sx={{ mt: 4, width: "100%" }} item>{element}</Grid>
+                        )}
+                    </Grid>
+                </Grid>
+            </Box>
+            <Divider/>
+            </div>
 
-           </Grid>
-        </Box>
-    )
-}
+    )}
