99cee5dcb91a3214a1d894aacb9219cd6523f8bd
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-04-30 22:53:01 -0500

    implementation autentication react

--- src/shared/NavBar.tsx
+++ src/shared/NavBar.tsx
@@ -1,15 +1,22 @@
 import React, {FC} from "react";
 import {AppBar, Badge, Box, Button, Container, Grid, IconButton, Stack, Toolbar, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
-import {useAppSelector} from "../redux/hooks";
+import {useAppDispatch, useAppSelector} from "../redux/hooks";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { CartComponent } from './Cart';
+import {logout} from "../redux/slices/auth.slice";
 
 
 export const NavBar: FC<{}> = () => {
     const navigate = useNavigate();
     const {isAuth} = useAppSelector(state => state.authReducer);
     const [open, setOpen] = React.useState<boolean>(false);
+
+    const dispatch = useAppDispatch();
+    const handleLogout = () => {
+    dispatch(logout())
+        navigate("/login")
+    }
     const handleStateViewDrawer = (state: string) => {
         setOpen((state) => !state);
     };
@@ -30,7 +37,7 @@ export const NavBar: FC<{}> = () => {
                         <Grid item>
                             {
                                 isAuth ?
-                                    <Button variant= "contained" >Logout</Button>
+                                    <Button variant= "contained" onClick={() => handleLogout()}>Logout</Button>
                                     : <Stack direction="row" spacing={2}>
                                         <IconButton
                                             color="primary"
